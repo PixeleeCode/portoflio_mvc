09c2fd269eb25bc64a6cd3516571864aae0790fc
Guillaume Bouyer <guillaume@proxilog.com>
2023-12-21 16:36:40 +0100

    Correction

--- src/Controller/ErrorController.php
+++ src/Controller/ErrorController.php
@@ -9,6 +9,7 @@ class ErrorController extends AbstractController
      */
     public function error404(): void
     {
+        http_response_code(404);
         $this->view('errors/404.php');
     }
 }
