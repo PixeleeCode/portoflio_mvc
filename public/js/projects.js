c0a72b7ba5d916d051af5b207a422b2e21bfb8b8
Guillaume Bouyer <guillaume@proxilog.com>
2024-01-04 14:34:37 +0100

    Extraction de la création des fiches produits dans une fonction JS

--- public/js/projects.js
+++ public/js/projects.js
@@ -10,23 +10,7 @@ fetch('/api/all/projects')
             document.querySelector('#loader').classList.add('d-none')
 
             projects.forEach(project => {
-                const template = document.querySelector('#project').content
-                const article = document.importNode(template, true)
-
-                // Gère le format date
-                const date = new Date(project.createdAt)
-
-                article.querySelector('h1').textContent = project.title
-                article.querySelector('small').textContent = `Posté le ${date.toLocaleDateString('fr')}`
-                article.querySelector('img').src = project.folderPreview
-                article.querySelector('img').alt = project.title
-                article.querySelector('p').textContent = `${project.description.slice(0, 75)}...`
-                article.querySelector('button').addEventListener('click', () => {
-                    // Appel une fonction pour récupérer les infos du projet
-                    readProject(project.id)
-                })
-
-                document.querySelector('#listing-projects').appendChild(article)
+                createCard(project)
             })
         }, 2000)
     })
@@ -40,34 +24,60 @@ function readProject(idProject)
     fetch(`/api/project?id=${idProject}`)
         .then(response => response.json())
         .then(project => {
-            const container = document.querySelector('#listing-projects')
-
-            // Permet de vider le contenu d'un élément HTML
-            container.textContent = ''
-
             // Modifie le titre du H1 de la page
             document.querySelector('h1').textContent = 'Mon projet'
 
             // Supprime la div "alerte" d'un utilisateur connecté
             document.querySelector('.alert-success').remove()
 
-            const template = document.querySelector('#project').content
-            const article = document.importNode(template, true)
-
-            // Gère le format date
-            const date = new Date(project.createdAt)
-
-            article.querySelector('h1').textContent = project.title
-            article.querySelector('small').textContent = `Posté le ${date.toLocaleDateString('fr')}`
-            article.querySelector('img').src = project.folderPreview
-            article.querySelector('img').alt = project.title
-            article.querySelector('p').textContent = project.description
-            article.querySelector('button').remove()
-
-            container.appendChild(article)
+            // Créer la fiche du produit
+            createCard(project, true)
 
             // Modifie l'URL du navigateur
             history.replaceState('', '', `/projet/details?id=${idProject}`)
         })
         .catch(error => console.log(error))
+}
+
+/**
+ * Création des cartes pour un projet
+ */
+function createCard(project, onlyOneProject = false)
+{
+    const container = document.querySelector('#listing-projects')
+
+    /**
+     * S'il s'agit d'un seul projet pour les infos,
+     * vide le contenu de la div affichant tous les projets
+     */
+    if (onlyOneProject) {
+        // Permet de vider le contenu d'un élément HTML
+        container.textContent = ''
+    }
+
+    const template = document.querySelector('#project').content
+    const article = document.importNode(template, true)
+
+    // Gère le format date
+    const date = new Date(project.createdAt)
+
+    article.querySelector('h1').textContent = project.title
+    article.querySelector('small').textContent = `Posté le ${date.toLocaleDateString('fr')}`
+    article.querySelector('img').src = project.folderPreview
+    article.querySelector('img').alt = project.title
+
+
+    // S'il s'agit d'un seul projet, on retire le bouton "En savoir plus..."
+    if (onlyOneProject) {
+        article.querySelector('p').textContent = project.description
+        article.querySelector('button').remove()
+    } else {
+        article.querySelector('p').textContent = `${project.description.slice(0, 75)}...`
+        // Appel une fonction pour récupérer les infos du projet
+        article.querySelector('button').addEventListener('click', () => {
+            readProject(project.id)
+        })
+    }
+
+    document.querySelector('#listing-projects').appendChild(article)
 }
